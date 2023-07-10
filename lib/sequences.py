9e52aa715831c37ed614b49cbe363df9ecceba5c
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-07-09 22:46:02 -0400

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fib_list = []
+    if length >= 1:
+        fib_list.append(0)
+    if length >= 2:
+        fib_list.append(1)
+    if length > 2:
+        a, b = 0, 1
+        for _ in range(length - 2):
+            a, b = b, a + b
+            fib_list.append(b)
+    print(fib_list)
     pass
